4399f47a614cd55253ac8773a1980dc24922895a
ytbbsq <89145754+ytbbsq@users.noreply.github.com>
2022-04-07 07:08:02 +0900

    test0407

--- Lec1.py
+++ Lec1.py
@@ -1,14 +1,12 @@
 # test
-print("test")
 
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
+print(type("test"))
+
 print("test")
 # a 「Ctrl + / 」（行コメント）
 # 「Shift + Alt + a 」（ブロックコメント)
 
-"""  """
+"""
+型の確認はtype()
+
+  """
