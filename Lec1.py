d31701c69eca04dd78d8bc3c939ad0976ae916c4
ytbbsq <89145754+ytbbsq@users.noreply.github.com>
2022-04-07 07:43:16 +0900

    0407

--- Lec1.py
+++ Lec1.py
@@ -1,8 +1,29 @@
 # test
 
-print(type("test"))
+def test_if():
+    a=100
+    b=50
+    if a > b:
+        a -= b
+    elif a < b:
+        a += b
+    else:
+        pass
+    print(a)
+    c= None
+    if not c:
+        print(c)
 
-print("test")
+def test_input():
+    age = int(input("何歳ですか？"))
+    casino_age = 18
+    if age >= casino_age:
+        print("どうぞお入りください")
+    else:
+        print("入場禁止です！！")
+
+test_input()
+
 # a 「Ctrl + / 」（行コメント）
 # 「Shift + Alt + a 」（ブロックコメント)
 
